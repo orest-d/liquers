41b52b40e0d5d37e14a09348756598315df51f21
Orest Dubay <orest-d@users.noreply.github.com>
2024-06-04 19:17:39 +0200

    pycall in progress

--- liquers-py/src/commands.rs
+++ liquers-py/src/commands.rs
@@ -1,7 +1,9 @@
 use liquers_core::context::ContextInterface;
 use liquers_core::state::State;
 use liquers_core::{register_command, value::ValueInterface};
+use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
+use pyo3::types::{PyDict, PyList, PyTuple};
 
 use crate::error::Error;
 use crate::value::Value;
@@ -53,6 +55,39 @@ fn pyprint(state: &State<Value>) -> Result<Value, Error> {
     Ok(Value::none())
 }
 
+fn json2py(py: Python, json: &serde_json::Value) -> PyResult<PyObject> {
+    match json{
+        serde_json::Value::Null => Ok(py.None()),
+        serde_json::Value::Bool(x) => Ok(x.into_py(py)),
+        serde_json::Value::Number(x) => {
+            if x.is_i64(){
+                Ok(x.as_i64().unwrap().into_py(py))
+            } else if x.is_u64(){
+                Ok(x.as_u64().unwrap().into_py(py))
+            } else if x.is_f64(){
+                Ok(x.as_f64().unwrap().into_py(py))
+            } else {
+                Err(PyErr::new::<PyException, _>("Number is not i64, u64 or f64"))
+            }
+        },
+        serde_json::Value::String(x) => Ok(x.into_py(py)),
+        serde_json::Value::Array(v) => {
+            let list = PyList::empty_bound(py);
+            for x in v.iter(){
+                list.append(json2py(py, x)?)?;
+            }
+            Ok(list.into())
+        }
+        serde_json::Value::Object(o) => {
+            let dict = PyDict::new_bound(py);
+            for (k,v) in o.iter(){
+                dict.set_item(k, json2py(py, v)?)?;
+            }
+            Ok(dict.into())
+        },
+    }
+}
+
 fn pycall(
     state: &State<Value>,
     arg: &mut liquers_core::commands::CommandArguments,
@@ -63,6 +98,8 @@ fn pycall(
     //let context_par = arg.pop_parameter()?;
     let module: String = arg.get(&context)?;
     let function: String = arg.get(&context)?;
+    //let pass_state: String = arg.get(&context)?;
+    /*
     let argv_par = arg.pop_parameter()?;
     let argv = argv_par
         .value()
@@ -81,8 +118,8 @@ fn pycall(
         .ok_or_else(|| {
             liquers_core::error::Error::general_error(format!("pycall argv is not an array"))
         })?;
-
-    println!("pycall {}.{}({:?})", module, function, argv);
+    */
+    println!("pycall {}.{}", module, function);
     for arg in arg.parameters.0.iter() {
         println!("arg: {:?}", arg);
     }
@@ -94,11 +131,25 @@ fn pycall(
         } else {
             PyModule::import(py, &*module)?
         };
+        let mut argv:Vec<PyObject> = Vec::new();
+        for (i,a) in arg.parameters.0.iter().enumerate().skip(2) {
+            match a{
+                liquers_core::plan::ParameterValue::DefaultValue(x) => argv.push(json2py(py, x)?),
+                liquers_core::plan::ParameterValue::DefaultLink(_) => todo!(),
+                liquers_core::plan::ParameterValue::ParameterValue(x, pos) => argv.push(json2py(py, x)?),
+                liquers_core::plan::ParameterValue::ParameterLink(_, _) => todo!(),
+                liquers_core::plan::ParameterValue::EnumLink(_, _) => todo!(),
+                liquers_core::plan::ParameterValue::Injected => todo!(),
+                liquers_core::plan::ParameterValue::None => todo!(),
+            }
+        }
+        let argv_pytuple = PyTuple::new_bound(py, argv);
+
         let f = m.getattr(&*function)?;
-        let res = f.call1((&s, argv))?;
+        let res = f.call1(argv_pytuple)?;
         //let builtins = PyModule::import_bound(py, "builtins")?;
         //builtins.getattr("print")?.call1(("PRINT <", &s, ">"))?;
-        Ok(())
+        return Ok(Value::Py { value: res.into() });
     })
     .map_err(|e: PyErr| {
         liquers_core::error::Error::general_error(format!("Python exception: {e}"))
