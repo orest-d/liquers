522030c40d80a8db810a4d83d2c62dafea05dc49
Orest Dubay <orest-d@users.noreply.github.com>
2025-03-08 20:01:53 +0100

    update to axum 0.8.1

--- liquers-axum/src/main.rs
+++ liquers-axum/src/main.rs
@@ -31,42 +31,42 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/liquer/q/*query", get(evaluate_handler))
+        .route("/liquer/q/{*query}", get(evaluate_handler))
         //.route("/liquer/submit/*query", get(submit_query))
         .route(
-            "/liquer/api/store/data/*query",
+            "/liquer/api/store/data/{*query}",
             get(crate::store_handlers::store_data_handler),
         )
         .route(
-            "/liquer/api/assets/data/*query",
+            "/liquer/api/assets/data/{*query}",
             get(crate::store_handlers::assets_data_handler),
         )
         //.route("/liquer/store/data/*query", post(crate::store_handlers::store_data_post_handler))
         .route(
-            "/liquer/api/store/metadata/*query",
+            "/liquer/api/store/metadata/{*query}",
             get(crate::store_handlers::store_metadata_handler),
         )
         //.route("/liquer/store/metadata/*query", post(crate::store_handlers::store_metadata_post_handler))
         .route(
-            "/liquer/api/store/upload/*query",
+            "/liquer/api/store/upload/{*query}",
             get(crate::store_handlers::upload_handler),
         )
         //.route("/liquer/store/upload/*query", post(crate::store_handlers::upload_post_handler))
         // /api/stored_metadata/QUERY (GET) ?
         .route(
-            "/liquer/api/store/remove/*query",
+            "/liquer/api/store/remove/{*query}",
             get(crate::store_handlers::remove_handler),
         )
         .route(
-            "/liquer/api/store/removedir/*query",
+            "/liquer/api/store/removedir/{*query}",
             get(crate::store_handlers::remove_handler),
         )
         .route(
-            "/liquer/api/store/contains/*query",
+            "/liquer/api/store/contains/{*query}",
             get(crate::store_handlers::remove_handler),
         )
         .route(
-            "/liquer/api/store/is_dir/*query",
+            "/liquer/api/store/is_dir/{*query}",
             get(crate::store_handlers::is_dir_handler),
         )
         .route(
@@ -74,15 +74,15 @@ async fn main() {
             get(crate::store_handlers::keys_handler),
         )
         .route(
-            "/liquer/api/store/listdir/*query",
+            "/liquer/api/store/listdir/{*query}",
             get(crate::store_handlers::listdir_handler),
         ) // TODO: support listdir_keys and listdir_keys_deep
         .route(
-            "/liquer/api/assets/listdir/*query",
+            "/liquer/api/assets/listdir/{*query}",
             get(crate::store_handlers::assets_listdir_handler),
         ) // TODO: support listdir_keys and listdir_keys_deep
         .route(
-            "/liquer/api/store/makedir/*query",
+            "/liquer/api/store/makedir/{*query}",
             get(crate::store_handlers::makedir_handler),
         )
         .with_state(state);
