88aa647f0baee8293f6a5dce826c6d1c0743f1d5
Orest Dubay <orest-d@users.noreply.github.com>
2025-09-18 14:08:32 +0200

    assync command registration macro testing

--- liquers-core/src/interpreter2.rs
+++ liquers-core/src/interpreter2.rs
@@ -304,7 +304,7 @@ mod tests {
             let what = state.try_into_string()?;
             Ok(Value::from(format!("{greet}, {what}!")))
         }
-        let mut cr = &mut env.command_registry;
+        let cr = &mut env.command_registry;
         register_command_v2!(cr, fn world(state) -> result).expect("register_command failed");
         register_command_v2!(cr, fn greet(state, greet: String = "Hello") -> result)
             .expect("register_command failed");
@@ -317,4 +317,32 @@ mod tests {
         assert_eq!(value, "Hello, world!");
         Ok(())
     }
+
+    #[tokio::test]
+    async fn test_async_hello_world() -> Result<(), Box<dyn std::error::Error>> {
+        type CommandEnvironment = SimpleEnvironment<Value>;
+        let mut env = SimpleEnvironment::<Value>::new();
+
+        // Register "hello" command
+        fn world(_state: &State<Value>) -> Result<Value, Error> {
+            Ok(Value::from("world"))
+        }
+        async fn greet(state: &State<Value>, greet: String) -> Result<Value, Error> {
+            let what = state.try_into_string()?;
+            Ok(Value::from(format!("{greet}, {what}!")))
+        }
+        let cr = &mut env.command_registry;
+        register_command_v2!(cr, fn world(state) -> result).expect("register_command failed");
+        //register_command_v2!(cr, async fn greet(state, greet: String = "Hello") -> result)
+        //    .expect("register_command failed");
+
+        let envref = env.to_ref();
+
+        let state = evaluate(envref.clone(), "world/greet", None).await?;
+
+        let value = state.try_into_string()?;
+        assert_eq!(value, "Hello, world!");
+        Ok(())
+    }
+
 }
---
+++ liquers-core/tests/async_hellow_world.rs
@@ -0,0 +1,37 @@
+use liquers_core::{
+    command_metadata::CommandKey,
+    context2::SimpleEnvironment,
+    context2::Context,
+    state::State,
+    value::Value,
+    error::Error,
+    interpreter2::evaluate,
+};
+use liquers_macro::*;
+
+#[tokio::test]
+async fn test_async_hello_world() -> Result<(), Box<dyn std::error::Error>> {
+    type CommandEnvironment = SimpleEnvironment<Value>;
+    let mut env = SimpleEnvironment::<Value>::new();
+
+    // Register "hello" command
+    fn world(_state: &State<Value>) -> Result<Value, Error> {
+        Ok(Value::from("world"))
+    }
+    async fn greet(state: &State<Value>, greet: String) -> Result<Value, Error> {
+        let what = state.try_into_string()?;
+        Ok(Value::from(format!("{greet}, {what}!")))
+    }
+    let cr = &mut env.command_registry;
+    register_command_v2!(cr, fn world(state) -> result).expect("register_command failed");
+    register_command_v2!(cr, async fn greet(state, greet: String = "Hello") -> result)
+         .expect("register_command failed");
+
+    let envref = env.to_ref();
+
+    let state = evaluate(envref.clone(), "world/greet", None).await?;
+
+    let value = state.try_into_string()?;
+    assert_eq!(value, "Hello, world!");
+    Ok(())
+}
